a3a0fd77d9a8514c0098c055e12822e3046e478d
shashank4214@gmail.com <shashank-gr>
2022-10-20 20:21:11 +0530

    first commit

---
+++ task3/app.js
@@ -0,0 +1,12 @@
+const title = document.querySelector("title");
+
+title.innerHTML = "item listner";
+const span = document.querySelector("#header-title span");
+span.style.display = "inline";
+// visibility: visible
+const header = document.querySelector("#main-header");
+header.style.borderBottom = "solid 3px #000";
+
+const addItems = document.querySelector(".title");
+addItems.style.textTransform = "uppercase";
+addItems.style.color = "green";
