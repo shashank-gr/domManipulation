ab9edc2dab6dc7ce180c9447e68e25e82c7fec72
shashank4214@gmail.com <shashank-gr>
2022-12-03 22:41:52 +0530

    usage of objects in TS

--- typescript/app.js
+++ typescript/app.js
@@ -13,11 +13,16 @@ const add = (num1, num2) => {
     }
     return +num1 + +num2;
 };
+//usage of Objects can define it is object and also the keys and there types
+function result(resultObj) {
+    console.log(`result ${resultObj.val} at ${resultObj.timeStamp}`);
+}
 btn.addEventListener("click", () => {
     const numSum = add(+ip1.value, +ip2.value);
     console.log("numbers sum", numSum);
     const stringConcat = add(ip1.value, ip2.value);
     console.log("string sum", stringConcat);
+    result({ val: numSum, timeStamp: new Date() });
 });
 // npm install -g typescript
 // create app.ts
--- typescript/app.ts
+++ typescript/app.ts
@@ -12,11 +12,19 @@ const add = (num1: number | string, num2: number | string) => {
   }
   return +num1 + +num2;
 };
+
+//usage of Objects can define it is object and also the keys and there types
+function result(resultObj: { val: number; timeStamp: Date }) {
+  console.log(`result ${resultObj.val} at ${resultObj.timeStamp}`);
+}
+
 btn.addEventListener("click", () => {
   const numSum = add(+ip1.value, +ip2.value);
   console.log("numbers sum", numSum);
   const stringConcat = add(ip1.value, ip2.value);
   console.log("string sum", stringConcat);
+
+  result({ val: numSum as number, timeStamp: new Date() });
 });
 
 // npm install -g typescript
